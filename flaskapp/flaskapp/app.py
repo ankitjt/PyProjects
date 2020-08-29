ebbcb94eb8c6fb0997fa5829d07160577c5b2f01
ankitjt <ankitjeetthakur@gmail.com>
2020-08-29 23:54:03 +0530

    adding templates

--- flaskapp/flaskapp/app.py
+++ flaskapp/flaskapp/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-     return "This is the home page of my flask app....."
+     return render_template("index.html")
 
 
 
