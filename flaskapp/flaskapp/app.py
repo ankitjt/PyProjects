cf49a7a2231b9cc98060bd176c46c0fffe6f2154
ankitjt <ankitjeetthakur@gmail.com>
2020-08-30 21:06:09 +0530

    adding bootstrap and flask template

--- flaskapp/flaskapp/app.py
+++ flaskapp/flaskapp/app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-     return render_template("home.html")
+     return render_template("home.html", title="Home Page")
 
+@app.route("/about")
+def about():
+     return render_template("about.html")
 
